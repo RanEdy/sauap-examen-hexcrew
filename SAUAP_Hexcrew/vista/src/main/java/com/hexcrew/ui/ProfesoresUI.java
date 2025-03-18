511ef8faa913e0fa2cd39c3c6259e3ae09f4172d
Xavier Ortiz <charlesxo17@gmail.com>
2025-03-17 18:12:36 -0700

    Sort
    Se agrego funcion de ordenacion

--- SAUAP_Hexcrew/vista/src/main/java/com/hexcrew/ui/ProfesoresUI.java
+++ SAUAP_Hexcrew/vista/src/main/java/com/hexcrew/ui/ProfesoresUI.java
@@ -11,30 +11,56 @@ import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import org.primefaces.model.SortOrder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.primefaces.model.SortMeta;
 
 /**
  *
  * @author charl
  */
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @Named("profesoresUI")
 @ViewScoped
 public class ProfesoresUI implements Serializable {
+
     private List<Profesor> listaProfesores;
-    public List<Profesor> getListaProfesores() { return listaProfesores; }
-    public void setListaProfesores(List<Profesor> lista) { listaProfesores = lista; }
-    
+
+    public List<Profesor> getListaProfesores() {
+        return listaProfesores;
+    }
+
+    public void setListaProfesores(List<Profesor> lista) {
+        listaProfesores = lista;
+    }
+
     @Inject
     private ProfesorHelper helper;
     
+
     @PostConstruct
-    public void init(){
-        listaProfesores = helper.obtenerLista();
+    public void init() {
+        listaProfesores = helper.obtenerLista(); 
+        
+        if(listaProfesores != null && !listaProfesores.isEmpty()){
+            listaProfesores.sort(Comparator.comparing(p -> p.getUsuario().getNombre()));
+        }
     }
-    
 }
+
+/**
+ * private List<SortMeta> sortBy;
+ * //listaProfesores.sort(Comparator.comparing(p -> p.getUsuario().getNombre()));
+        //sortBy = new ArrayList<>();
+        //sortBy.add(SortMeta.builder()
+                //.field("usuario.nombre")
+                //.order(SortOrder.ASCENDING)
+                //.build()); 
+ */
